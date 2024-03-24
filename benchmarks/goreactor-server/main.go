5d5044421c205a3d7b4b874c86346e22b7a30213
markity <jiangxu@redrock.team>
2024-03-24 11:01:40 +0800

    fix: benchmark syntax bug

--- benchmarks/goreactor-server/main.go
+++ benchmarks/goreactor-server/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	evloop := eventloop.NewEventLoop(0)
+	evloop := eventloop.NewEventLoop()
 	server := goreactor.NewTCPServer(evloop, "127.0.0.1:8000", 0, goreactor.RoundRobin())
 	server.SetMessageCallback(func(t goreactor.TCPConnection, b buffer.Buffer) {
 		t.Send(b.Peek())
